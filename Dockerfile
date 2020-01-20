b4592df1ccbadc67c3ff9ca10c6eeba4dcea7e18
Pascal Vizeli <pvizeli@syshack.ch>
2020-01-21 00:06:45 +0100

    Fix issues

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,7 @@ RUN apt-get update && apt-get install -y --no-install-recommends \
         python-numpy \
         tigervnc-standalone-server \
         ca-certificates \
+        procps \
     && rm -rf /var/lib/apt/lists/* \
     && git clone --depth 1 -b v${NOVNC_VERSION} https://github.com/novnc/noVNC \
     && git clone --depth 1 -b v${WEBSOCKIFY_VERSION} https://github.com/novnc/websockify noVNC/utils/websockify \
--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 TMP_FOLDER=$(mktemp -d)
 export XDG_RUNTIME_DIR="${TMP_FOLDER}"
 export DISPLAY=":0"
+export E_NO_DBUS_SESSION=1
 
 tigervncserver \
     -name "Hass.io - Desktop" \
@@ -10,7 +11,13 @@ tigervncserver \
     -depth 24 \
     -localhost yes \
     -SecurityTypes None \
+    -xstartup /usr/bin/enlightenment_start \
     "${DISPLAY}" \
     &
 
+# wait for port 5900
+sleep 5
+
 ./usr/src/noVNC/utils/launch.sh --vnc localhost:5900 --listen 8099
+
+sleep 600
